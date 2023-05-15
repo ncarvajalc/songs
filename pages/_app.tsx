0370c70d9ecdf7ee5530cace4fc796a641edb168
Nicolás Carvajal <n.carvajalc@uniandes.edu.co>
2023-05-14 20:16:57 -0500

    Added full screen mode in iPhone

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,14 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
